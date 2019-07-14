80659824707f3e196049ee15c3d0b185cb922558
Tim Austin <tim@neenjaw.com>
2019-07-14 09:11:12 -0600

    More succinct

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,13 +1,7 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
-export const value = ([color1, color2]) => {
-  const color1Value = COLORS.indexOf(color1);
-  const color2Value = COLORS.indexOf(color2);
+export const value = ([...colors]) => {
+  colors.map(color => COLORS.indexOf(color));
 
-  return (color1Value * 10) + color2Value;
+  return (color[0] * 10) + color[1];
 };
