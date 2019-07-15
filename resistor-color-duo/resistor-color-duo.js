b8e6f5099b60c422fd160f44bd1b178a8b6741d6
Tim Austin <tim@neenjaw.com>
2019-07-15 00:42:15 -0600

    resisterduo refacter

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,7 @@
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
 export const value = ([...colors]) => {
-  colors.map(color => COLORS.indexOf(color));
+  colors = colors.map(color => COLORS.indexOf(color));
 
-  return (color[0] * 10) + color[1];
+  return (colors[0] * 10) + colors[1];
 };
