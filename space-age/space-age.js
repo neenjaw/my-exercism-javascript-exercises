5c5f1e36df930f21748e3ea1003da16ad8baf5fc
Tim Austin <tim@neenjaw.com>
2019-08-09 08:59:22 -0600

    soln

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,8 +1,31 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/*
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+Earth: orbital period 365.25 Earth days, or 31557600 seconds
+Mercury: orbital period 0.2408467 Earth years
+Venus: orbital period 0.61519726 Earth years
+Mars: orbital period 1.8808158 Earth years
+Jupiter: orbital period 11.862615 Earth years
+Saturn: orbital period 29.447498 Earth years
+Uranus: orbital period 84.016846 Earth years
+Neptune: orbital period 164.79132 Earth years
+
+*/
+
+export const orbitalPeriod = {
+  earth:   1,
+  mercury: 0.2408467,
+  venus:   0.61519726,
+  mars:    1.8808158,
+  jupiter: 11.862615,
+  saturn:  29.447498,
+  uranus:  84.016846,
+  neptune: 164.79132
+};
+
+export const age = (planet, earthAgeInSeconds) => {
+  const convertSecondsToYears = (seconds) => seconds / 60 / 60 / 24 / 365.25;
+  const convertToPlanetYears = (planet, earthYears) => earthYears / orbitalPeriod[planet];
+  const roundToTwoPlaces = (number) => Math.round(number * 100) / 100;
+
+  return roundToTwoPlaces(convertToPlanetYears(planet, convertSecondsToYears(earthAgeInSeconds)))
 };
