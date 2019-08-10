d4cc87a3a2929b1d9b915e88127ab45ace7e42ef
Tim Austin <tim@neenjaw.com>
2019-08-10 00:44:29 -0600

    pangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+import { strict } from "assert";
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+const isLetter = (char) => char.match(/[a-z]/i)
+
+export const isPangram = (candidateString) => {
+  const letterMap = candidateString
+                      .split("")
+                      .reduce((map, char) => {
+                        if (isLetter(char)) {
+                          map.set(char.toLowerCase(), true);
+                        }
+
+                        return map;
+                      }, new Map());
+
+  return letterMap.size == 26
 };
