ce3febb78b25e70e194448a23fa628fb5cb2ebfa
Tim Austin <tim@neenjaw.com>
2019-08-12 01:36:53 -0600

    spread rather than split

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -3,11 +3,8 @@ import { strict } from "assert";
 const isLetter = (char) => char.match(/[a-z]/i)
 
 export const isPangram = (candidateString) => {
-  const candidateLetters = candidateString
-                            .toLowerCase()
-                            .split("")
-                            .filter(isLetter);
-
+  const candidateNormalized = candidateString.toLowerCase()
+  const candidateLetters = [...candidateNormalized].filter(isLetter);
   const letterSet = new Set(candidateLetters);
 
   return letterSet.size == 26
