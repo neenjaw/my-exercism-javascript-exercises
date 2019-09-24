1b355409e66598de4172d6bbab30ec3c0330975d
Tim Austin <tim@neenjaw.com>
2019-09-23 21:37:24 -0600

    leap rev tri

--- leap/leap.js
+++ leap/leap.js
@@ -1,6 +1,6 @@
 export const isLeap = (year) => {
-  if (year % 4 != 0) return false;
-  if (year % 2000 == 0) return true;
-  if (year % 100 == 0) return false;
+  if (year % 4 !== 0) return false;
+  if (year % 2000 === 0) return true;
+  if (year % 100 === 0) return false;
   return true;
 };
--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,14 +1,29 @@
-//
-// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const INVALID_SIDE_ERR = 'sides must be of positive length';
+const TRIANGLE_INEQUALITY_ERR = 'sides violate triangle inequality';
 
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(...sides) {
+    this.sides = sides;
   }
 
   kind() {
-    throw new Error("Remove this statement and implement this function");
+    const sides = [...this.sides];
+    const sorter = (a,b) => a-b;
+    sides.sort(sorter)
+
+    if (sides[0] <= 0) {
+      throw new Error(INVALID_SIDE_ERR);
+    }
+    if ((sides[0] + sides[1]) < sides[2]) {
+      throw new Error(TRIANGLE_INEQUALITY_ERR);
+    }
+
+    const [a,b,c] = [...sides];
+
+    if (a === b && b === c && a === c) return 'equilateral';
+    if (a === b) return 'isosceles';
+    if (b === c) return 'isosceles';
+
+    return 'scalene'
   }
 }
