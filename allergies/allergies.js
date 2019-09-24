509192ff1719c20dcc409ffb0574cca971fd2ee4
Tim Austin <tim@neenjaw.com>
2019-09-24 16:14:56 -0600

    allergies

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -1,13 +1,30 @@
+const allergens = {
+  eggs: 1,
+  peanuts: 2,
+  shellfish: 4,
+  strawberries: 8,
+  tomatoes: 16,
+  chocolate: 32,
+  pollen: 64,
+  cats: 128,
+}
+
 export class Allergies {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(score) {
+    this.allergenList = [];
+
+    for (let [key, value] of Object.entries(allergens)) {
+      if ((value & score) === value) {
+        this.allergenList.push(key)
+      }
+    }
   }
 
   list() {
-    throw new Error("Remove this statement and implement this function");
+    return [...this.allergenList]
   }
 
-  allergicTo() {
-    throw new Error("Remove this statement and implement this function");
+  allergicTo(allergen) {
+    return (this.allergenList.indexOf(allergen)) !== -1
   }
 }
