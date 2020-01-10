78ffd5c245522702f20238e71d1ce41aadc80a51
Tim Austin <tim@neenjaw.com>
2020-01-10 01:23:37 -0600

    lazy

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -1,74 +1,37 @@
-function getRandomIntInclusive(min, max) {
+const TOTAL_NUMBER_OF_LETTERS = 26 * 26;
+const TOTAL_NUMBER_OF_NUMBERS = 10 * 10 * 10;
+
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-// function getRandomIntegerDigit() {
-//   return getRandomIntInclusive(0, 9)
-// }
+function getRandomLetters() {
+  const letterNumber = getRandomInt(0, TOTAL_NUMBER_OF_LETTERS)
+  const letterValues = [Math.floor(letterNumber/26)%26, letterNumber%26]
+  const [a, b] = letterValues.map(l => String.fromCharCode(l+65))
 
-// function getRandomLetter() {
-//   const a = 65 // 'A'
-//   const z = 90 // 'Z'
-
-//   return String.fromCharCode(getRandomIntInclusive(a, z))
-// }
-
-// function getName() {
-//   return [
-//     getRandomLetter(),
-//     getRandomLetter(),
-//     getRandomIntegerDigit(),
-//     getRandomIntegerDigit(),
-//     getRandomIntegerDigit(),
-//   ].join('')
-// }
-
-function range(start, end) {
-  return (function*() {
-    for (let index = start; index <= end; index++) {
-      yield index;
-    }
-  })()
+  return `${a}${b}`
 }
 
-function alphaRange() {
-  return range(65, 90)
+function getRandomNumbers() {
+  const number = getRandomInt(0, TOTAL_NUMBER_OF_NUMBERS)
+  return `${number}`.padStart(3, '0')
 }
 
-function digitRange() {
-  return range(0, 9)
+function getRandomName() {
+  return getRandomLetters() + getRandomNumbers()
 }
 
-function* generateNames() {
-  for (let a of alphaRange()) {
-    a = String.fromCharCode(a)
-    for (let b of alphaRange()) {
-      b = String.fromCharCode(b)
-      for (const c of digitRange()) {
-        for (const d of digitRange()) {
-          for (const e of digitRange()) {
-            // console.log({a,b,c,d,e})
-
-            yield `${a}${b}${c}${d}${e}`
-          }
-        }
-      }
-    }
-  }
-}
-
-const robotRegistry = new Set();
-
-for (const name of generateNames()) {
-  robotRegistry.add(name)
-}
+const nameRegistry = new Set()
 
 function getUniqueName() {
-  const i = getRandomIntInclusive(0, robotRegistry.size-1)
-  const name = [...robotRegistry.values()][i]
-  robotRegistry.delete(name)
+  let name
+  do {
+    name = getRandomName()
+  } while (nameRegistry.has(name));
+  nameRegistry.add(name)
   return name
 }
 
@@ -86,4 +49,4 @@ export class Robot {
   }
 }
 
-Robot.releaseNames = () => { };
+Robot.releaseNames = () => nameRegistry.clear();
