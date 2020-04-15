142a8be1bafb1f99412aa6aa0c0e217a184bf56c
Tim Austin <tim@neenjaw.com>
2020-04-15 16:14:06 -0600

    class based

--- secret-handshake/secret-handshake.js
+++ secret-handshake/secret-handshake.js
@@ -1,31 +1,54 @@
-class Action {
-   constructor(description, code) {
-      this.description = description
-      this.code = code
-   }
+export const commands = (code) => {
+  const handshake = new Handshake(code)
+  return handshake.actions
 }
 
-const actions = [
-  new Action('wink', 0b1),
-  new Action('double blink', 0b10),
-  new Action('close your eyes', 0b100),
-  new Action('jump', 0b1000)
-]
+class Handshake {
+  constructor(code) {
+    const isActionEncoded = getCodeCheckFunction(code)
 
-const commands = (code) => {
-  const commands = actions
-    .filter(checkCode(code))
-    .map(getDescription)
+    const actionCollector = (isActionEncoded(Action.reverseAction()))
+                          ? Array.prototype.unshift
+                          : Array.prototype.push
 
-  if (checkCode(code)(0b10000)) commands.reverse()
+    this._actions = []
+    Action
+      .handshakeActions()
+      .forEach(getHandshakeActions(isActionEncoded, actionCollector, this._actions))
+  }
 
-  return commands
+  get actions() {
+    return [...this._actions]
+  }
 }
 
-const checkCode = (code) => {
+const getCodeCheckFunction = (code) => {
   return (action) => (code & action.code) !== 0
 }
 
-const getDescription = (action) => {
-  return action.description
+const getHandshakeActions = (isActionEncodedFn, actionCollectorFn, actionsCollection) => {
+  return (action) => {
+    if (isActionEncodedFn(action))
+        actionCollectorFn.call(actionsCollection, action.description)
+  }
+}
+
+class Action {
+  constructor(description, code) {
+    this.description = description
+    this.code = code
+  }
+
+  static handshakeActions() {
+    return [
+      new Action('wink', 0b1),
+      new Action('double blink', 0b10),
+      new Action('close your eyes', 0b100),
+      new Action('jump', 0b1000)
+    ]
+  }
+
+  static reverseAction() {
+    return new Action('reverse', 0b10000)
+  }
 }
